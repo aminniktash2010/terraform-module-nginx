8a3d91f8edba52e2c6b7ca6edd2fad02829d0dbe
aminniktash2010 <80291034+aminniktash2010@users.noreply.github.com>
2021-04-23 14:37:20 -0400

    1

--- nginx-app-variables.tf
+++ nginx-app-variables.tf
@@ -18,7 +18,7 @@ variable "prefix" {
 variable "location" {
   type        = string
   description = "Azure region where the resource group will be created"
-  default     = "north europe"
+  default     = "canadacentral"
 }
 
 # azure region shortname
