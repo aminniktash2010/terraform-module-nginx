b58d6859adca7fb1f0a31e49639c0dfc9feb47cf
aminniktash2010 <80291034+aminniktash2010@users.noreply.github.com>
2021-04-23 14:52:08 -0400

    5

--- vm-nginx-output.tf
+++ vm-nginx-output.tf
@@ -15,12 +15,11 @@ output "nginx_vm_ip_address" {
 output "nginx_vm_admin_username" {
   description = "Administrator Username for the Virtual Machine"
   value       = var.nginx_admin_username
-  #sensitive   = false
 }
 
 output "nginx_vm_admin_password" {
   description = "Administrator Password for the Virtual Machine"
   value       = random_password.nginx-vm-password.result
-  #sensitive   = false
+  
 }
 
